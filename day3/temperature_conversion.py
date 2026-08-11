3802341e1d3f02cef3dab8997b5046ab4109a9d8
eswarasaiguruvelli <eswarasaiguruvelli@gmail.com>
2026-08-11 21:21:20 +0530

    day3 added

---
+++ day3/temperature_conversion.py
@@ -0,0 +1,4 @@
+#Temperature data conversion give a temperature in celsius as a floating point value convert it into fahrenheit the result be printed with two decimal places
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"Temperature in Fahrenheit: {fahrenheit:.2f}") 
