c02ed335d69b312bf0faeeb234c092579e0e8a97
eswarasaiguruvelli <eswarasaiguruvelli@gmail.com>
2026-08-16 22:27:25 +0530

    code added

---
+++ day7/arm.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number: "))
+
+original = n
+digits = len(str(n))
+total = 0
+
+while n > 0:
+    digit = n % 10
+    total = total + digit ** digits
+    n = n // 10
+
+if total == original:
+    print("Armstrong number")
+else:
+    print("Not an Armstrong number")
